71452900bf006b20806e86d03212fddd6492300b
elyesT <elyes.tabbane@learningplanetinstitute.org>
2024-11-02 09:38:46 +0100

    added new questions to the end survey

--- scripts/end_survey.js
+++ scripts/end_survey.js
@@ -39,7 +39,7 @@ const options_vviq_fr = [
   "L'image est parfaitement nette, aussi précise et vivace qu'une véritable perception",
 ];
 
-// ------ Questions
+// ------ Questions VVIQ
 const questions_fr = [
   'Le contour exact de son visage, de sa tête, de ses épaules et de son corps',
   'La manière dont cet.te ami.e tient sa tête, les postures de son corps, etc.',
@@ -78,17 +78,49 @@ const questions_eng = [
   'A strong wind blows on the trees and on the lake, causing waves.',
 ];
 
+// ------ Questions experiment
+const questions_exp_fr = [
+  'Je visualisais les formes géométriques tracées par les points',
+  'Je me souvenais de la structure abstraites de la séquence (groupe de points, répétitions, miroirs, etc.)',
+  'Je transformais les points en note de musique',
+  "J'utilisais mes doigts ou mon corps comme aide de mémoire",
+];
+
+const questions_exp_eng = [
+  'I visualized the geometric shapes drawn by the points',
+  'I remembered the abstract structure of the sequence (group of points, repetitions, mirrors, etc.)',
+  'I transformed the points into musical notes',
+  'I used my fingers or body as a memory support',
+];
+
+const option_exp_fr = [
+  "Pas du tout d'accord",
+  "Pas d'accord",
+  'Ne sais pas',
+  "D'accord",
+  "Entièrement d'accord",
+];
+
+const option_exp_eng = [
+  'Strongly disagree',
+  'Disagree',
+  "I don't know",
+  'Agree',
+  'Strongly Agree',
+];
 // ------ Instructions
 
 const instructions_eng = [
-  "Think of some relative or friend whom you frequently see (but who is not with you at present), and consider carefully the picture that comes before your mind's eye. Then rate the following items:",
+  'First, a few questions about your strategies during the experiment you just did.',
+  "Now, we'll perform a visual imagery test. Think of some relative or friend who you frequently see (but who is not with you at present), and consider carefully the picture that comes before your mind's eye. Then rate the following items:",
   "Visualize a rising sun. Consider carefully the picture that comes before your mind's eye. Then rate the following items:",
   "Think of the front of a shop to which you often go. Consider carefully the picture that comes before your mind's eye. Then rate the following items:",
   "Finally, think of a country scene which involves trees, mountains and a lake. Consider carefully the picture that comes before your mind's eye. Then rate the following items:",
 ];
 
 const instructions_fr = [
-  "Imaginez attentivement l'image d'un.e ami.e que vous voyez fréquemment et qui n'est pas présent.e en ce moment. Puis évaluez les caractéristiques suivantes.",
+  "Tout d'abord, quelques questions sur les stratégies utilisées pendant l'expérience que vous venez de faire",
+  "Maintenant, nous allons faire un test d'imagination visuelle. Imaginez attentivement l'image d'un.e ami.e que vous voyez fréquemment et qui n'est pas présent.e en ce moment. Puis évaluez les caractéristiques suivantes.",
   "Imaginez le lever du soleil. Analysez attentivement l'image qui apparaît.",
   "Imaginez un magasin dans lequel vous allez souvent. Analysez en détail l'image qui vous vient à l'esprit.",
   "Imaginez une scène de campagne avec des arbres, des montagnes, un lac. Analysez, en détail, les images que vous viennent à l'esprit.",
@@ -99,18 +131,41 @@ if (lan_selected == 'fr') {
   var questions_survey = questions_fr;
   var instructions = instructions_fr;
   var options_vviq = options_vviq_fr;
+  var questions_strategy = questions_exp_fr;
+  var options_exp = option_exp_fr;
 } else {
   var questions_survey = questions_eng;
   var instructions = instructions_eng;
   var options_vviq = options_vviq_eng;
+  var questions_strategy = questions_exp_eng;
+  var options_exp = option_exp_eng;
 }
+
+// ----- Combine Questions
+
+// Map `questions_exp` to have a similar structure to `vviq_questions`
+// const experiment_questions = questions_exp.map((question) => ({
+//   question,
+//   options: options_exp,
+// }));
+
+// // Use `Object.assign` to add `experiment_questions` at the beginning of `questions`
+// const questions = Object.assign([], experiment_questions, questions_survey);
 
 // ----- Questions
 
-const questions = questions_survey.map((question) => ({
+const vviq_questions = questions_survey.map((question) => ({
   question,
   options: options_vviq,
 }));
+
+// Add questions about the experiment itself
+const exp_questions = questions_strategy.map((question) => ({
+  question,
+  options: options_exp,
+}));
+
+const questions = exp_questions.concat(vviq_questions);
 
 //--------------------
 function loadQuestions() {
