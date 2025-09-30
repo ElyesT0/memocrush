2020074d92aa6a8ffb78ed97318b448c1f2652ed
ElyesT0 <elyes.tabbane@gmail.com>
2025-09-30 13:01:46 +0200

    added a sanity check function : sanity_check_data_JSON()

--- 1-Scripts/stimulation/scripts/send_post.js
+++ 1-Scripts/stimulation/scripts/send_post.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const saveParticipantData = (
   experiment_name_loc,
@@ -6,8 +6,8 @@ const saveParticipantData = (
   participantData_loc
 ) => {
   var participantID_loc = initial_participantID;
-  console.log('sending data');
-  console.log('sent object POST : ', {
+  console.log("sending data");
+  console.log("sent object POST : ", {
     experiment_name: experiment_name_loc,
     participantID: participantID_loc,
     participantData: participantData_loc,
@@ -20,9 +20,40 @@ const saveParticipantData = (
       participantData: participantData_loc,
     })
     .then((response) => {
-      console.log('Data successfully saved:', response.data);
+      console.log("Data successfully saved:", response.data);
     })
     .catch((error) => {
-      console.error('Error saving data:', error);
+      console.error("Error saving data:", error);
     });
 };
+
+// SUB-PART
+// -----------------------
+// SANITY CHECKS FUNCTIONS
+// -----------------------
+function sanity_check_data_JSON() {
+  const data = {
+    sequences_temp_tags: all_sequences_temp_tags,
+    sequences_geom_tags: all_sequences_geom_tags,
+    sequences_original: original_sequence_train_test,
+    sequences_shown: sequence_train_test,
+  };
+
+  // helper for download
+  function downloadJSON(obj, filename) {
+    const blob = new Blob([JSON.stringify(obj, null, 2)], {
+      type: "application/json",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
+  }
+
+  // download both sessions separately
+  downloadJSON(data, "data_sanityCheck.json");
+}
